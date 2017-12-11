84ba90c546de435c4cd4c146cfebf9aa8a896b85
Le Vu Hoang <rainyhoang@gmail.com>
2017-12-11 10:05:19 -0500

    ok

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers(
-id int AUTO_INCREMENT,
+id int AUTO_INCREMENT NOT NULL,
 PRIMARY KEY (id),
 burger_name varchar(50) NOT NULL,
 devoured BOOLEAN default false,
